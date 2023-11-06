2b34ad9d29cdbddae2e448c37f62f2993da67bb9
Sreejith2 <99952612+Sreejith2@users.noreply.github.com>
2023-11-06 22:36:50 +0530

    added pages

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const app = express();
 app.get("/",(req,res)=>{
     res.send("Hello world");
 });
+app.get("/about",(req,res)=>{
+    res.send("Welcome to about page");
+});
+
+app.get("/contact",(req,res)=>{
+    res.send("Welcome to contact page");
+});
+
+app.get("/blog",(req,res)=>{
+    res.send("Welcome to blog page");
+});
 
 app.listen(port,(res,req)=>{
     console.log(`Server initiated at http://localhost:${port}`);
